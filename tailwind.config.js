61936c70208acaaac481936fb11b53bb3a89c744
Raaz <raazkatwal5@gmail.com>
2024-12-02 01:07:48 +0545

    added tailwind through npm

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'accent': '#00FE9B',
+        'background': '#1D1C22',
+      },
+    },
+  },
+  plugins: [],
+}
+
