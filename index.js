2037bbcb0a6b560ece0ede7d7ff98104c2ce259a
Raaz <raazkatwal5@gmail.com>
2024-12-02 19:17:50 +0545

    added contact me section

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ function checkScroll() {
 window.onload = checkScroll;
 
 window.addEventListener("scroll", checkScroll);
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('animContainer'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'https://assets9.lottiefiles.com/packages/lf20_5n8yfkac.json'
+})
