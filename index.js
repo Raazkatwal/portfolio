b31cdc933c4011b1249ba80ea45125cd7462c4ac
Raaz <raazkatwal5@gmail.com>
2024-12-03 15:05:52 +0545

    made responsive

--- index.js
+++ index.js
@@ -14,10 +14,21 @@ window.onload = checkScroll;
 
 window.addEventListener("scroll", checkScroll);
 
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('animContainer'),
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'https://assets9.lottiefiles.com/packages/lf20_5n8yfkac.json'
-})
+const mbl_nav = document.querySelector("#mbl-nav");
+const menu = document.querySelector(".fa-bars");
+const close_menu = document.querySelector(".fa-x");
+const mbl_links = document.querySelectorAll(".mbl-links");
+menu.addEventListener("click", () => {
+  mbl_nav.classList.remove("-right-[70rem]");
+  mbl_nav.classList.add("right-0");
+});
+close_menu.addEventListener("click", () => {
+  mbl_nav.classList.remove("right-0");
+  mbl_nav.classList.add("-right-[70rem]");
+});
+mbl_links.forEach((e) => {
+  e.addEventListener("click", () => {
+    mbl_nav.classList.remove("right-0");
+    mbl_nav.classList.add("-right-[70rem]");
+  });
+});
